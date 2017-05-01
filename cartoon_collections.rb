6c1ff430e2cc67239b2b7d4848abe92464c063a7
Rathi <github email address>
2017-05-01 18:45:27 -0400

    re did this lab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,23 +1,25 @@
 def roll_call_dwarves(arr_name)
-arr_name.each.with_index do |name,idx|
-  puts "#{idx+1}#{name}"
+  arr_name.each.with_index do |name,idx|
+    puts "#{idx+1}#{name}"
+  end
 end
 
-end
+
 
 def summon_captain_planet(arr_capitalize)
-  arr_capitalize.map{|name| name.capitalize+"!"}
+  arr_capitalize.map do
+    |name| "#{name.capitalize}!"
+  end
 end
 
 def long_planeteer_calls(arr)
-  arr.any?{|name| name.length > 4}
-
+  arr.any? {|name| name.length > 4}
 end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
   arr.each do |name|
-  return name if  cheese_types.include?(name)
+    return name if cheese_types.include?(name)
   end
   nil
 end
